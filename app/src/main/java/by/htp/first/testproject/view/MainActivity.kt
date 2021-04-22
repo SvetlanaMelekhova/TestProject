69e7563e1b54669307ca19d2d6133576ed778720
SvetlanaMelekhova <71405460+SvetlanaMelekhova@users.noreply.github.com>
2021-04-22 21:07:34 +0300

    some changes

--- app/src/main/java/by/htp/first/testproject/view/MainActivity.kt
+++ app/src/main/java/by/htp/first/testproject/view/MainActivity.kt
@@ -1,5 +1,6 @@
 package by.htp.first.testproject.view
 
+import android.content.res.ColorStateList
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -9,16 +10,31 @@ import androidx.fragment.app.FragmentTransaction
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
 import by.htp.first.testproject.R
-import by.htp.first.testproject.databinding.ActivityMainBinding
+
+import com.kyleduo.switchbutton.SwitchButton
 
 class MainActivity : AppCompatActivity() {
 
     /*private lateinit var binding: ActivityMainBinding
     private lateinit var rootView: View*/
 
+    //var switch : SwitchButton? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_report)
+        setContentView(R.layout.fragment_settings)
+
+       // switch = findViewById<SwitchButton>(R.id.swOnOff)
+
+      //  switch?.setOnClickListener{
+
+            /*if(switch?.isChecked!!){
+                switch?.setBackColorRes(R.color.green)
+            }
+            else{
+                switch?.setBackColorRes(R.color.gray)
+            }*/
+        }
     }
         /*rootView = findViewById(R.id.rootMainLayout)
         binding = ActivityMainBinding.bind(rootView)
@@ -55,4 +71,4 @@ class MainActivity : AppCompatActivity() {
             replace(R.id.fragmentContainer, fragment)
             addToBackStack(null)
         }*/
-    }
+
